d4ce93b4bbea46bb3b93d97c61bae8a4864c1257
noman ali <nomanali.beyondvision@gmail.com>
2022-02-09 16:16:32 +0500

    Add project files.

---
+++ Controllers/AddDealsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Deals;
+
+namespace Deals.Controllers
+{
+    public class AddDealsController : Controller
+    {
+        private POSEntities db = new POSEntities();
+
+        // GET: AddDeals
+        public ActionResult Index()
+        {
+            var addDeals = db.AddDeals.Include(a => a.deal).Include(a => a.item);
+            return View(addDeals.ToList());
+        }
+
+        // GET: AddDeals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AddDeal addDeal = db.AddDeals.Find(id);
+            if (addDeal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addDeal);
+        }
+
+        // GET: AddDeals/Create
+        public ActionResult Create()
+        {
+            ViewBag.d_id = new SelectList(db.deals, "d_id", "dname");
+            ViewBag.it_id = new SelectList(db.items, "it_id", "pname");
+            return View();
+        }
+
+        // POST: AddDeals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,d_id,it_id")] AddDeal addDeal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AddDeals.Add(addDeal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.d_id = new SelectList(db.deals, "d_id", "dname", addDeal.d_id);
+            ViewBag.it_id = new SelectList(db.items, "it_id", "pname", addDeal.it_id);
+            return View(addDeal);
+        }
+
+        // GET: AddDeals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AddDeal addDeal = db.AddDeals.Find(id);
+            if (addDeal == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.d_id = new SelectList(db.deals, "d_id", "dname", addDeal.d_id);
+            ViewBag.it_id = new SelectList(db.items, "it_id", "pname", addDeal.it_id);
+            return View(addDeal);
+        }
+
+        // POST: AddDeals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,d_id,it_id")] AddDeal addDeal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(addDeal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.d_id = new SelectList(db.deals, "d_id", "dname", addDeal.d_id);
+            ViewBag.it_id = new SelectList(db.items, "it_id", "pname", addDeal.it_id);
+            return View(addDeal);
+        }
+
+        // GET: AddDeals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AddDeal addDeal = db.AddDeals.Find(id);
+            if (addDeal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addDeal);
+        }
+
+        // POST: AddDeals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AddDeal addDeal = db.AddDeals.Find(id);
+            db.AddDeals.Remove(addDeal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/dealsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Deals;
+
+namespace Deals.Controllers
+{
+    public class dealsController : Controller
+    {
+        private POSEntities db = new POSEntities();
+
+        // GET: deals
+        public ActionResult Index()
+        {
+            return View(db.deals.ToList());
+        }
+
+        // GET: deals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            deal deal = db.deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // GET: deals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: deals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "d_id,dname,Price")] deal deal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.deals.Add(deal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(deal);
+        }
+
+        // GET: deals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            deal deal = db.deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // POST: deals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "d_id,dname,Price")] deal deal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deal);
+        }
+
+        // GET: deals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            deal deal = db.deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // POST: deals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            deal deal = db.deals.Find(id);
+            db.deals.Remove(deal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
